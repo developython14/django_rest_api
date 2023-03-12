3ec5119cd1d1a4fd9b7fb4c75706797d9a405063
developython14 <djmel1955@gmail.com>
2023-03-12 16:10:15 +0100

    initialistaion od the projects

--- mycar/urls.py
+++ mycar/urls.py
@@ -38,5 +38,6 @@ router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls'))
 ]
