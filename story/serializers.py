8892cabda67716b264e51876f85fc793b08202bd
developython14 <djmel1955@gmail.com>
2023-04-25 04:11:10 +0200

    story api

--- story/serializers.py
+++ story/serializers.py
@@ -4,15 +4,16 @@ from rest_framework import serializers
 
 
 class story_image_serializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.IntegerField(read_only=True)
     class Meta:
         model = story_image
         fields = [ 'id', 'image',
-                 ]
+                ]
 
 
 class storySerializer(serializers.HyperlinkedModelSerializer):
     id = serializers.IntegerField(read_only=True)
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=story_image.objects.all())
+    tracks = serializers.StringRelatedField(many=True)
     class Meta:
         model = story
         fields = ['id','created', 'page_de_garde', 'order' , 'snippets']
