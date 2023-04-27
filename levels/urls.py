edd98b55469a007edb287c4edd2b9a9e0b075d87
developython14 <djmel1955@gmail.com>
2023-04-27 18:19:52 +0200

    story api

--- levels/urls.py
+++ levels/urls.py
@@ -6,12 +6,14 @@ urlpatterns =[
     path('levels/',get_levels, name="levels"),
     path('post_level/',post_levels, name="post_level"),
     path('put_level/',put_levels, name="put_level"),
+    path('delete_level/',delete_levels, name="delete_level"),
 ]
 
 
-#url patterfn about filliers
+#url patterfn about fillierss
 urlpatterns +=[
-    path('levels/',get_levels, name="levels"),
-    path('post_level/',post_levels, name="post_level"),
-    path('put_level/',put_levels, name="put_level"),
+    path('filliers/',get_levels, name="filiers"),
+    path('post_filiere/',post_levels, name="post_filiere"),
+    path('put_filiere/',put_levels, name="post_filiere"),
+    path('delete_filiere/',delete_levels, name="delete_filiere"),
 ]
--- levels/views.py
+++ levels/views.py
@@ -5,7 +5,7 @@ from .models import *
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+# Create levels views here.
 
 def get_levels( request):
     people = levels.objects.all().values()
@@ -22,7 +22,7 @@ def post_levels( request):
     abre = data['abre']
     new = levels(title = title , abre =abre , order = order)
     new.save()
-    return HttpResponse({'message': 'succesfly '})
+    return HttpResponse("updated succeffluy")
 
 @csrf_exempt
 def put_levels(request):
@@ -42,4 +42,66 @@ def put_levels(request):
         pass
     # <view logic>
     return HttpResponse("updated succeffluy")
+
+@csrf_exempt
+def delete_levels(request):
+    data = request.POST
+    id  = data['id']
+    try : 
+        people = levels.objects.all().filter(id=id).delete()
+    except:
+        pass
+    # <view logic>
+    return HttpResponse("deleted succeffluy")
+    
+
+
+# Create filliere views here.
+
+def get_filliers( request):
+    people = Filieres.objects.all().values()
+    people = list(people)
+    return JsonResponse(people ,safe=False)
+
+
+@csrf_exempt
+def post_levels( request):
+        # <view logic>
+    data = request.POST
+    order = data['order']
+    title = data['title']
+    abre = data['abre']
+    new = levels(title = title , abre =abre , order = order)
+    new.save()
+    return HttpResponse("updated succeffluy")
+
+@csrf_exempt
+def put_levels(request):
+    data = request.POST
+    id  = data['id']
+    try : 
+        people = levels.objects.all().filter(id=id).update(title= request.POST['title'])
+    except:
+        pass
+    try : 
+        people = levels.objects.all().filter(id=id).update(order= request.POST['order'])
+    except:
+        pass
+    try : 
+        people = levels.objects.all().filter(id=id).update(abre= request.POST['abre'])
+    except:
+        pass
+    # <view logic>
+    return HttpResponse("updated succeffluy")
+
+@csrf_exempt
+def delete_levels(request):
+    data = request.POST
+    id  = data['id']
+    try : 
+        people = levels.objects.all().filter(id=id).delete()
+    except:
+        pass
+    # <view logic>
+    return HttpResponse("deleted succeffluy")
     
