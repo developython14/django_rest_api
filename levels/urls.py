73866bfea77de3be0ba1503fc28447cc925c0c4b
developython14 <djmel1955@gmail.com>
2023-04-27 05:47:33 +0200

    story api

--- levels/urls.py
+++ levels/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import *
 urlpatterns =[
-    path('level/',Level.as_view()),
+    path('levels/',get_levels, name="levels"),
+    path('post_level/',post_levels, name="post_level"),
+    path('put_level/',put_levels, name="put_level"),
 ]
--- levels/views.py
+++ levels/views.py
@@ -3,21 +3,25 @@ from django.http import HttpResponse
 from django.views import View
 from .models import *
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
-class Level(View):
-    def get(self, request):
-        people = levels.objects.all()
-        people = list(people)
-        return JsonResponse(people ,safe=False)
-    def post(self, request):
+def get_levels( request):
+    people = levels.objects.all()
+    people = list(people)
+    return JsonResponse(people ,safe=False)
+    
+@csrf_exempt
+def post_levels( request):
         # <view logic>
-        data = request.data
-        print('hadi data ')
-        print(data)
-        return HttpResponse("result")
-    def put(self, request):
-        # <view logic>
-        return HttpResponse("result")
+    data = request.data
+    print('hadi data ')
+    print(data)
+    return HttpResponse("result")
+
+@csrf_exempt
+def put_levels(request):
+    # <view logic>
+    return HttpResponse("result")
     
